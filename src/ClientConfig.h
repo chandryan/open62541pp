c1df174b936cb678573cd3ec82a0068e44afb419
Christian Granzin <christian.granzin@gmail.com>
2024-07-13 06:45:11 -0400

    fix: Take over new pointer to non-default logger for the event loop

--- src/ClientConfig.h
+++ src/ClientConfig.h
@@ -17,6 +17,11 @@ public:
     void setLogger(Logger logger) {
         if (logger) {
             logger_.assign(LoggerAdapter(std::move(logger)));
+            // From version 1.4 the event loop gets created with a copy of the pointer to the
+            // logger. It needs to be updated when we set a non-default logger.
+#if UAPP_OPEN62541_VER_GE(1, 4)
+            config_.eventLoop->logger = config_.logging;
+#endif
         }
     }
 
